1979b35b2fe5703861d15e1e965d814ef421a391
Mem0Real <juniourmimo@gmail.com>
2025-01-28 04:13:49 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import "./index.css";
+
+function AnimatedBox() {
+	const boxRef = useRef(null);
+
+	useFrame(() => {
+		// boxRef.current.rotation.x += 0.005;
+		boxRef.current.rotation.y += 0.005;
+		// boxRef.current.rotation.z += 0.005;
+	});
+
+	return (
+		<mesh ref={boxRef}>
+			<boxGeometry args={[2, 2, 2]} />
+			<meshStandardMaterial color={0x00bfff} />
+		</mesh>
+	);
+}
+
+function App() {
+	return (
+		<div id="canvas-container">
+			<Canvas>
+				<AnimatedBox />
+				<directionalLight position={[4, 2, 3]} />
+			</Canvas>
+		</div>
+	);
+}
+
+export default App;
